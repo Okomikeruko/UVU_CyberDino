5a8569e0fd0104de44b544c067caa471e3f6f463
Michael Farnsworth <thefarnz83@gmail.com>
2014-04-25 10:48:25 -0600

    Spino Roll VFX

---
+++ UnityProject-CyberDino/Assets/Resources/VFX/SpinoRollEffect/RadiusRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadiusRotation : MonoBehaviour {
+	
+	void Start () 
+	{
+		rotationDirection *= rotationSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(rotationDirection);
+	}
+	
+	void OnGUI()
+	{
+		rotationSpeed = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), rotationSpeed, 0.0F, 10.0F);
+	}
+	
+	
+	public float rotationSpeed = 1.0f;
+	public Vector3 rotationDirection;
+}
+//script created by Li Liu
