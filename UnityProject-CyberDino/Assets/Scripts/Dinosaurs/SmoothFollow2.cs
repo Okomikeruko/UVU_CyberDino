adbe503343cb729cf643824af843e4b1ed4ee6d2
Lee Whittaker <whittakerlee81@gmail.com>
2014-04-29 17:46:24 -0600

    Heavily modified the Camera SmoothFollow2.cs to always orient upward and to vary the distance behind each dino.

--- UnityProject-CyberDino/Assets/Scripts/Dinosaurs/SmoothFollow2.cs
+++ UnityProject-CyberDino/Assets/Scripts/Dinosaurs/SmoothFollow2.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class SmoothFollow2 : MonoBehaviour {
@@ -7,14 +8,39 @@ public class SmoothFollow2 : MonoBehaviour {
 	public float height = 3.0f;
 	public float damping = 5.0f;
 	public bool smoothRotation = true;
+	private bool once = true;
 	public float rotationDamping = 10.0f;
-	
+
+	private enum DinoList {Diloph, Hesp, Raptor, Spino, TRex, Troodon};
+	private enum CameraIndex {distance, height, damping};
+
+	private float[,] CameraSettings = new float[6,3]{
+		/*Diloph Camera Settings*/{15,10,5},
+		/*Hesp Camera Settings*/{10,10,5},
+		/*Raptor Camera Settings*/{12,10,5},
+		/*Spino Camera Settings*/{35,12.5F,5},
+		/*TRex Camera Settings*/{25,12.5F,5},
+		/*Troodon Camera Settings*/{10,10,5},
+	};
+
 	void Update () {
+		if (once && target != null)
+		{
+			int dino = (int)(DinoList)Enum.Parse(typeof(DinoList), target.name.Remove(target.name.IndexOf ('(')));
+
+			distance = CameraSettings[dino, (int)CameraIndex.distance];
+			height = CameraSettings[dino, (int)CameraIndex.height];
+			damping = CameraSettings[dino, (int)CameraIndex.damping];
+
+			once = false;
+		}
+
 		Vector3 wantedPosition = target.TransformPoint(0, height, -distance);
 		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
 		
 		if (smoothRotation) {
-			Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+			Vector3 lookatme = target.position + new Vector3(0,height,0);
+			Quaternion wantedRotation = Quaternion.LookRotation(lookatme - transform.position, Vector3.up);
 			transform.rotation = Quaternion.Slerp (transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
 		}
 		
