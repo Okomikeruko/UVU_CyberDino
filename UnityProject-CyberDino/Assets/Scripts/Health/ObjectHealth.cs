2728dd6bc9545de776c61edb7d4dc474b952d6b0
Myles Nielsen <Me.N787@gmail.com>
2014-09-09 20:40:45 -0600

    Track (Damagable objects and building placement) and VFX changes

---
+++ UnityProject-CyberDino/Assets/Scripts/Health/ObjectHealth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHealth : Health {
+	
+	override public void OnDamage()
+	{
+		Debug.Log (gameObject.name + " took damage.");
+	}
+	
+	override public void OnHeal()
+	{
+		Debug.Log (gameObject.name + " was healed.");
+	}
+	
+	override public void OnDeath()
+	{
+		// Destroy / Reset turret
+		Debug.Log (gameObject.name + " died ... x_x");
+		Destroy (this.gameObject);
+	}
+	
+}
---
+++ UnityProject-CyberDino/Assets/Scripts/VFX/destroyTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyTimer : MonoBehaviour {
+
+	private float timer = 0.1f;
+	public float deathTime = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+		timer = deathTime;
+		this.particleSystem.Play ();
+		Debug.Log (this.particleSystem.isPlaying);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer <= 0.0f) {
+			Destroy (this.gameObject);
+		}
+		timer -= Time.deltaTime;
+	}
+}
