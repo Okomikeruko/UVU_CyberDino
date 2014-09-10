8713a01c5770f39c9edcd64f284aec096f2a0181
Myles <Me.N787@gmail.com>
2014-09-10 10:25:26 -0600

    Dust working over network.

--- UnityProject-CyberDino/Assets/Scripts/VFX/dusttrail.cs
+++ UnityProject-CyberDino/Assets/Scripts/VFX/dusttrail.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class dusttrail : MonoBehaviour {
 
+	public GameObject networkHandlerObject;
+	private NetworkGameHandler networkHandler;
+	private int playerID;
+
+	void Start ()
+	{
+		var playerInfo = networkHandler.GetMyInfo();
+		playerID = int.Parse(Network.player.ToString());
+	}
+
 	void step(string foot)
 	{
-		GameObject newSmoke = (GameObject)Instantiate ((GameObject)Resources.Load ("dustTrail"), this.transform.position, Quaternion.LookRotation (this.transform.forward));
+		GameObject newSmoke = (GameObject)Network.Instantiate ((GameObject)Resources.Load ("dustTrail"), this.transform.position, Quaternion.LookRotation (this.transform.forward), playerID);
 	}
 
 }
