40a3d7e17eb502ab8a3165f770334091fbedfa7f
Lee Whittaker <lee@21zero.com>
2014-09-25 18:07:32 -0600

    Linked animation to Spino BuzzSaw. Created "Duration" as editable values for both BuzzSaw and TeleportSlam

--- UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Weapons/Diloph/TeleportSlam.cs
+++ UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Weapons/Diloph/TeleportSlam.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class TeleportSlam : MeleeAttack {
 
+	[SerializeField]
+	private float Duration;
+
 	public override void Fire ()
 	{
 		Debug.Log("Teleport Slam!");
 		/* Animation trigger added by Lee*/
-		StartCoroutine (slam(0.5F));
+		StartCoroutine (slam(Duration));
 	}
 
 	private IEnumerator slam (float seconds)
--- UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Weapons/Spino/BuzzSaw.cs
+++ UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Weapons/Spino/BuzzSaw.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 
 public class BuzzSaw : MeleeAttack {
-	
+
+	[SerializeField]
+	private float duration;
+
 	public override void Fire ()
 	{
 		Debug.Log ("Buzz Saw!");
+		/* Animation trigger added by Lee*/
+		StartCoroutine (spin (duration));
+	}
+	
+	private IEnumerator spin (float seconds)
+	{
+		NetworkAnimations netanim = GetComponentInChildren<NetworkAnimations>();
+		netanim.AnimSetMelee ("Melee", true);
+		yield return new WaitForSeconds(seconds);
+		netanim.AnimSetMelee ("Melee", false);
 	}
 	
 }
