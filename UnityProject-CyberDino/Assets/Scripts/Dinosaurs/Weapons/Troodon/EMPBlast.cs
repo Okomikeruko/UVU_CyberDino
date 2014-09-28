a9da9f292c3686331de8f35c364b27b4f1e27f0a
Dan Burgener <uvudan@gmail.com>
2014-09-28 08:01:10 -0600

    Created EMPBlast. Waiting for animation and ability to drop items.

--- UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Weapons/Troodon/EMPBlast.cs
+++ UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Weapons/Troodon/EMPBlast.cs
@@ -1,11 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EMPBlast : Bomb {
+
+	public const float DAMAGE = 10.0f;
 
 	public override void Fire ()
 	{
 		Debug.Log ("EMP Blast!");
-	}
+		float gatheredHealth = 0;
+		var DinoRacers = GameObject.FindGameObjectsWithTag("Dino");
+		var AiRacers = GameObject.FindGameObjectsWithTag("Ai");
+		List<GameObject> racers = new List<GameObject>();
+		racers.AddRange(DinoRacers);
+		racers.AddRange(AiRacers);
+		foreach (var racer in racers){
+			if (racer == this.gameObject){
+				continue;
+			}
 
+			//Lightning strikes all other racers
+			Debug.Log("Play lightening strike animation");
+			//Play strike animation
+
+			//Take up to 10 health from those racers
+			var dinosaurhHealth = racer.GetComponent<DinosaurHealth>();
+			if (dinosaurhHealth.Current >= DAMAGE){
+				gatheredHealth += DAMAGE;
+				dinosaurhHealth.Damage(DAMAGE);
+			}else{
+				gatheredHealth += dinosaurhHealth.Current;
+				dinosaurhHealth.Damage(dinosaurhHealth.Current);
+			}
+
+
+			//All other racers drop all of their pickups on the track, which can be picked up again later.
+			//racer.GetComponent<Inventory>().DropAll();
+			Debug.Log("Have racers drop all items");
+		}
+		//Add health to firing player's health, up to max health
+		this.GetComponent<DinosaurHealth>().Heal(gatheredHealth);
+
+	}
 }
