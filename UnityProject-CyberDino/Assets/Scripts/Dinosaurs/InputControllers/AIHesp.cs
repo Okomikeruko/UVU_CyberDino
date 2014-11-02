30cd8eca4337cca65ef069e134938d2169bfb4b1
DarrenMoody <moodytoons@gmail.com>
2014-11-02 13:19:18 -0700

    Placing AIHesp script. No implementation yet.

---
+++ UnityProject-CyberDino/Assets/Scripts/Dinosaurs/InputControllers/AIHesp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIHesp : MonoBehaviour {
+
+
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
