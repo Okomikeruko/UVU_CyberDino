def5d0421531838de2e124cc2a7b7059e6677f94
Christian Nielsen <cnielsenxt@gmail.com>
2014-11-08 12:28:35 -0700

    Added Footstep sound effects to the light and medium sized dinosaurs.

--- UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Motion/AnimationTriggers/AnimationTriggers.cs
+++ UnityProject-CyberDino/Assets/Scripts/Dinosaurs/Motion/AnimationTriggers/AnimationTriggers.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AnimationTriggers : MonoBehaviour {
+
+	[SerializeField]
+	private List<AudioSource> Footsteps;
 
 	void Start () {
 	
@@ -9,6 +13,8 @@ public class AnimationTriggers : MonoBehaviour {
 
 	void OnStep (string Foot) {
 		// Play footstep sound
+		var stepFX = Footsteps[Random.Range(0, Footsteps.Count)];
+		stepFX.Play();
 	}
 
 }
