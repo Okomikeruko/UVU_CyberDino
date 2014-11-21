76d9a64995597950f4836ec64f3e22407620518c
DarrenMoody <moodytoons@gmail.com>
2014-11-21 13:48:48 -0700

    Finishing the AIPickUp.cs script

--- UnityProject-CyberDino/Assets/Scripts/PickUps/AIPickUp.cs
+++ UnityProject-CyberDino/Assets/Scripts/PickUps/AIPickUp.cs
@@ -1,25 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class AIPickUp : MonoBehaviour {
+public class AIPickUp : MonoBehaviour
+{
 
-	private PickUpTypes currentType;
-	
-	private Camera m_Camera;
-	GameObject myContainer;
-	
-	void Awake(){
-		m_Camera = Camera.main;
-	}
-	void Start(){
+    private Inventory inv;
 
-	}
-	//When an ai player hits the pickup section, they are assigned a weapon
-	void OnTriggerEnter(Collider other)
-	{
-		if(other.tag == "aiPickUp")
-		{
-			//choose the pickup that you want to add for any reason and add it to the inventory. See PickUp.cs for how to do that.
-		}
-	}
+    // Finishing up this script and connecting it to the in game objects called
+    // "AIPickUp" in the dumbell track rather than attaching it to Ai dinos 
+    // When an AIPickUp collides with an Ai dino, the Ai dino gets a weapon pickup
+    // This will happen automatically for all Ai dinos- love Darren
+    void OnTriggerEnter(Collider other)
+    {
+        int randy = Random.Range(1, 10);
+        // 1 in 10 chance of getting a weapon pickup
+        // If they get a weapon every time the game becomes pure MADNESS! :-D Is fun though
+        if (other.tag == "Ai" && randy == 3)
+        {
+            inv = other.gameObject.GetComponent<Inventory>();
+            inv.AddPickUp(PickUpTypes.Weapon);
+            Debug.Log(other.name + " got a WEAPON");
+        }
+    }
 }
